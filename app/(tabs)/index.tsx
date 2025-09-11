e5025fa897b2b4a8f8d59d9c9e4601c8e61c24d9
Pedroo07 <pedrohenriquemoalive@gmail.com>
2025-09-10 21:49:49 -0300

    creating dashboard page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,82 +1,141 @@
-import React from "react";
-import { View, Text, StyleSheet, Pressable } from "react-native";
-import { TextInput } from "react-native-paper";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import { Button } from "react-native-paper";
+import { ArrowDown, ArrowUpRight, Equal } from "lucide-react-native";
 
 export default function HomeScreen() {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.logo}>Logo</Text>
-        <Text style={styles.logo}>B-finances</Text>
+      <View>
+        <Text style={styles.title}>Dashboard</Text>
+        <View style={styles.head}>
+          <View style={styles.result}>
+            <ArrowDown color="white" size={40} />
+            <Text style={styles.text}>Expense</Text>
+            <Text style={styles.text}>$500,00</Text>
+          </View>
+          <View style={[styles.result, { backgroundColor: "#0f766e" }]}>
+            <ArrowUpRight color="white" size={40} />
+            <Text style={styles.text}>Income</Text>
+            <Text style={styles.text}>$2,500</Text>
+          </View>
+          <View style={[styles.result, { backgroundColor: "#918d8d" }]}>
+            <Equal color="white" size={40} />
+            <Text style={styles.text}>Total</Text>
+            <Text style={styles.text}>$2,000</Text>
+          </View>
+        </View>
       </View>
-    
-      <View style={styles.form}>
-        <TextInput
-          label="Email"
-          value={email}
-          onChangeText={setEmail}
-          style={styles.input}
-          mode="outlined"
-          activeOutlineColor="#3b82f6"
-        />
-
-        <TextInput
-          label="Password"
-          value={password}
-          onChangeText={setPassword}
-          style={styles.input}
-          mode="outlined"
-          secureTextEntry
-          activeOutlineColor="#3b82f6"
-        />
-
-        <Pressable style={styles.button}>
-          <Text style={styles.buttonText}>Enter</Text>
-        </Pressable>
+      <View
+        style={{
+          borderStyle: "solid",
+          borderWidth: 1,
+          borderColor: "#555252",
+          margin: 10,
+        }}
+      >
+        <View>
+          <Text style={styles.titleList}>Transaction List</Text>
+          <FlatList
+            contentContainerStyle={{
+              flexGrow: 1,
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+            horizontal
+            style={{
+              ...styles.list,
+              borderBottomWidth: 1,
+              borderColor: "#555252",
+              paddingRight: 30,
+            }}
+            data={[{ key: "Description" }, { key: "Value" }, { key: "Date" }]}
+            renderItem={({ item }) => (
+              <Text
+                style={[
+                  styles.listItem,
+                  { color: "#5a5757", fontWeight: "semibold" },
+                ]}
+              >
+                {item.key}
+              </Text>
+            )}
+          />
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+        >
+          <FlatList
+            style={styles.list}
+            data={[{ key: "salary" }, { key: "Gorcieries" }, { key: "rent" }]}
+            renderItem={({ item }) => (
+              <Text style={styles.listItem}>{item.key}</Text>
+            )}
+          />
+          <FlatList
+            data={[{ key: "2,500,00" }, { key: "-150,00" }, { key: "-50,00" }]}
+            renderItem={({ item }) => (
+              <Text style={styles.listItem}>{item.key}</Text>
+            )}
+          />
+          <FlatList
+            style={styles.list}
+            data={[{ key: "apr20" }, { key: "apr18" }, { key: "apr01" }]}
+            renderItem={({ item }) => (
+              <Text style={styles.listItem}>{item.key}</Text>
+            )}
+          />
+        </View>
       </View>
+      <Button>+</Button>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    boxSizing: "border-box",
     flex: 1,
-    backgroundColor: "#1e293b", 
+    backgroundColor: "#fff",
+    gap: 12,
   },
-  header: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    padding: 40,
-    backgroundColor: "#475569",
-    alignItems: "center",
-  },
-  logo: {
+  text: {
     color: "#fff",
-    fontSize: 20,
+  },
+  head: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    padding: 8,
+  },
+  title: {
     fontWeight: "bold",
+    fontSize: 36,
+    color: "#1e293b",
+    padding: 20,
   },
-  form: {
-    flex: 1,
-    justifyContent: "center",
-    paddingHorizontal: 20,
+  titleList: {
+    fontSize: 24,
+    padding: 12,
+    fontWeight: "semibold",
+    textTransform: "uppercase",
   },
-  input: {
-    marginBottom: 15,
-  },
-  button: {
-    backgroundColor: "#3b82f6",
-    padding: 15,
+  result: {
+    alignItems: "center",
+    backgroundColor: "#e00808",
+    padding: 14,
     borderRadius: 8,
-    alignItems: "center",
   },
-  buttonText: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: "bold",
+  list: {
+    display: "flex",
+    padding: 4,
+  },
+  listItem: {
+    padding: 10,
+    fontSize: 18,
   },
 });
--- app/login.tsx
+++ app/login.tsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React from "react";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { TextInput } from "react-native-paper";
 
-const login = () => {
+export default function HomeScreen() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
   return (
-    <div>login</div>
-  )
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.logo}>Logo</Text>
+        <Text style={styles.logo}>B-finances</Text>
+      </View>
+    
+      <View style={styles.form}>
+        <TextInput
+          label="Email"
+          value={email}
+          onChangeText={setEmail}
+          style={styles.input}
+          mode="outlined"
+          activeOutlineColor="#3b82f6"
+        />
+
+        <TextInput
+          label="Password"
+          value={password}
+          onChangeText={setPassword}
+          style={styles.input}
+          mode="outlined"
+          secureTextEntry
+          activeOutlineColor="#3b82f6"
+        />
+
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Enter</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
 }
 
-export default login
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#1e293b", 
+  },
+  header: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    padding: 40,
+    backgroundColor: "#475569",
+    alignItems: "center",
+  },
+  logo: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  form: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 20,
+  },
+  input: {
+    marginBottom: 15,
+  },
+  button: {
+    backgroundColor: "#3b82f6",
+    padding: 15,
+    borderRadius: 8,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
